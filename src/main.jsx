c4df2aeddd85b38927942df311cc09824b3c19bd
tonegeniuss <tonegeniuss@gmail.com>
2025-07-01 12:14:22 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>ToneGeniuss is Live 🎵</h1>
+      <p>More features coming soon.</p>
+    </div>
+  </React.StrictMode>
+)
